6bffe458dac12ef89852d40a7523df2ae7be36ca
wgjtyu <wgjtyu@gmail.com>
2013-09-09 15:59:22 +0800

    use regexp to replace path

--- src/index.coffee
+++ src/index.coffee
@@ -4,16 +4,13 @@ module.exports = class HtmlTempalate
     extension: 'html'
 
     constructor: (config) ->
-        if config.plugins.ngTemplate != undefined
+        if config?.plugins?.ngTemplate != undefined
             @templateDir = config.plugins.ngTemplate.template
-            if @templateDir.slice(-1) != "/"
-                @templateDir=@templateDir + "/"
         else
             @templateDir = undefined
 
     compile: (data, path, callback) ->
-        if (@templateDir != undefined) and (path.indexOf(@templateDir) != -1)
-            path = path.slice(@templateDir.length)
+        path = path.replace @templateDir, ''
         head = "<script type='text/ng-template' id='"+path+"'>\n"
         tail = "</script>\n"
         callback null, head + data + tail
--- test/plugin_test.js
+++ test/plugin_test.js
@@ -5,32 +5,28 @@ describe('Plugin', function() {
     plugin = new Plugin({});
   });
 
-  it('should init', function() {
-    var config = {ngTemplate : { template: 'src/template' }};
-    plugin = new Plugin( config );
-    expect(plugin.templateDir).to.equal('src/template/');
-
-    var config = {ngTemplate : { template: 'src/template/' }};
-    plugin = new Plugin( config );
-    expect(plugin.templateDir).to.equal('src/template/');
-  });
-
-  it('should compile with full path', function() {
-    var path = 'src/template/test.html';
+  it('compile without templateDir', function() {
+    var path = 'dev/test/test.html';
     var data = 'html';
     var callback = function(error, html){
-      expect(html).to.equal("<script type='text/ng-template' id='src/template/test.html'>\nhtml</script>\n");
+      expect(html).to.equal("<script type='text/ng-template' id='dev/test/test.html'>\nhtml</script>\n");
     }
     plugin.compile(data, path, callback);
   });
 
   it('should compile with trim path', function() {
-    var path = 'src/template/test.html';
+    plugin.templateDir = /^(app|dev)\//
+
+    var path = undefined;
     var data = 'html';
     var callback = function(error, html){
-      expect(html).to.equal("<script type='text/ng-template' id='test.html'>\nhtml</script>\n");
+      expect(html).to.equal("<script type='text/ng-template' id='login/login.html'>\nhtml</script>\n");
     }
-    plugin.templateDir = 'src/template/';
+
+    path = 'app/login/login.html';
+    plugin.compile(data, path, callback);
+
+    path = 'dev/login/login.html';
     plugin.compile(data, path, callback);
   });
 
